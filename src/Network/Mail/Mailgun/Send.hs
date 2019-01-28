ab29d29cebeba9dfc76e92f760ae1a38f27fbafa
davean <davean@xkcd.com>
2019-01-28 05:19:20 -0500

    Stream sending.

--- src/Network/Mail/Mailgun/Send.hs
+++ src/Network/Mail/Mailgun/Send.hs
@@ -8,6 +8,7 @@ module Network.Mail.Mailgun.Send
  , MailgunSendOptions(..)
  , tags, dkim, deliverAt, track, trackClicks, trackOpens, templateVariables
  , send
+ , sending
  ) where
 
 import           Control.Lens
@@ -17,14 +18,18 @@ import qualified Data.Aeson as JS
 import           Data.Aeson.Lens
 import           Data.Ascii (CIAscii)
 import qualified Data.Ascii as ASCII
+import qualified Data.ByteString.Lazy as BSL
+import           Data.Machine
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Text (Text)
 import qualified Data.Text as T
+import           Data.These
 import           Data.Time
 import           Network.Mail.Mailgun.API
 import           Network.Mail.Mailgun.Config
-import           Network.Mail.Mime (Address, Mail, renderMail', renderAddress)
+import           Network.Mail.Mime (Address, addressEmail, Mail, renderMail', renderAddress)
+import           Network.Mime
 import           Network.Wreq
 import qualified Network.Wreq as HTTP
 import           Text.Printf
@@ -65,15 +70,20 @@ yesNo :: Text -> Bool -> HTTP.Part
 yesNo t True  = partText t "yes"
 yesNo t False = partText t "no"
 
-mgsoAsMultipart :: MailgunSendOptions -> [HTTP.Part]
-mgsoAsMultipart o = mconcat
- [ o^..tags.each.to (partBS "o:tag" . ASCII.ciToByteString)
- , [partText "o:dkim" (if o^.dkim then "yes" else "no")]
- , o^..deliverAt.each.to (partString "o:deliverytime" . formatTime defaultTimeLocale "%a, %e %b %Y %T %z")
- , [o^.track.to (yesNo "o:tracking")]
- , [o^.trackClicks.to clickTrackFormPart]
- , [o^.trackOpens.to (yesNo "o:tracking-opens")]
- , map (\(k, v) -> partLBS ("v:" `T.append` k) (JS.encode v)) (o^.templateVariables.to Map.toList)
+mgsoAsMultipart :: Bool -> Maybe MailgunSendOptions -> [HTTP.Part]
+mgsoAsMultipart test mo = mconcat $
+ [ [yesNo "o:testmode" test]
+ , maybe [] (\o -> mconcat
+     [ o^..tags.each.to (partBS "o:tag" . ASCII.ciToByteString)
+     , [partText "o:dkim" (if o^.dkim then "yes" else "no")]
+     , o^..deliverAt.each.to (partString "o:deliverytime" .
+                                         formatTime defaultTimeLocale "%a, %e %b %Y %T %z")
+     , [o^.track.to (yesNo "o:tracking")]
+     , [o^.trackClicks.to clickTrackFormPart]
+     , [o^.trackOpens.to (yesNo "o:tracking-opens")]
+     , map (\(k, v) -> partLBS ("v:" `T.append` k) (JS.encode v))
+           (o^.templateVariables.to Map.toList)
+     ]) mo
  ]
 
 -- | Sends a given email.
@@ -82,122 +92,72 @@ send :: (HasMailgunConfig c, MonadIO m, MonadThrow m, MonadReader c m)
 send mo dests m = do
   test  <- view mailgunTestMode
   rndrd <- liftIO $! renderMail' m
-  call (  MGPost (printf "/v3/%s/messages.mime") [] . mconcat $
-    [ [yesNo "o:testmode" test]
-    , maybe [] mgsoAsMultipart mo
-    , [partLBS "message" rndrd & partFileName .~ Just "message.mime"
-                               ]
+  call (MGPost (printf "/v3/%s/messages.mime") [] . mconcat $
+    [ mgsoAsMultipart test mo
+    , [partLBS "message" rndrd & partFileName .~ Just "message.mime"]
     , map (partText "to" . renderAddress) dests
     ]) (^?key "id"._JSON)
 
 {- This has to be the form API, which leaves less control over attachments?
+ - Its unclear if the MIME API  allows recipient-variables.
+  -}
+
+type FromAddress  = Address
+type CcAddresses  = [Address]
+type BccAddresses = [Address]
+type Subject      = Text
+type HtmlBody     = Text
+type TextBody     = Text
+type InlineAttachments = [Attachment]
+type Attachments       = [Attachment]
+type Attachment        = (MimeType, Maybe FileName, BSL.ByteString)
+
+attachmentToMutli :: Bool -> Attachment -> Part
+attachmentToMutli inline (contentType, mFileName, body) =
+  partLBS (if inline then "inline" else "attachment") body
+    & partFileName .~ (fmap T.unpack mFileName)
+    & partContentType .~ Just contentType
+
 -- | Takes an email, ignoring the to addresses, and sends it to all the
 --   addresses streamed in, paramterized by the JS.Values which can be used
 --   in the templating.
 sending :: (HasMailgunConfig c, MonadIO m, MonadThrow m, MonadReader c m
           ,JS.ToJSON t)
-        => Maybe MailgunSendOptions -> Mail
+        => Maybe MailgunSendOptions
+        -> FromAddress
+        -> CcAddresses
+        -> BccAddresses
+        -> Subject
+        -> These HtmlBody TextBody
+        -> InlineAttachments
+        -> Attachments
         -> ProcessT m (Address, t) MessageID
-sending mo m = buffered 1000 ~> sendBatch ~> flattened
+sending mo fromAddr ccAddr bccAddr subj theseBodies inline attach =
+  buffered 1000 ~> sendBatch
   where
     sendBatch = preplan $ do
       test  <- view mailgunTestMode
-      rndrd <- liftIO $! renderMail' m
-      call (sendReq test mo rndrd $
-              (map (partText "to" . renderAddress) (mailTo m)))
-           (^?key "id"._JSON)
--}
-
---renderAddress :: Address -> Text
---renderAddress (Address (Nothing) addr) = addr
---renderAddress (Address (Just nm) addr) = printf "%s <%s>" nm addr
-
-{-
-class IsEmail t where
-  emailFrom    :: Lens'  t  Address
-  emailCC      :: Lens'  t [Address]
-  emailBCC     :: Lens'  t [Address]
-  emailHeaders :: Lens'  t  Headers
-  emailParts   :: Lens'  t [Alternatives]
-
-instance IsEmail Mail where
-  emailFrom    = lens mailFrom (\m a -> m {mailFrom=a})
-  emailCC      = lens mailCc  (\m c -> m {mailCc=c})
-  emailBCC     = lens mailBcc (\m b -> m {mailBcc=b})
-  emailHeaders = lens mailHeaders (\m h -> m {mailHeaders=h})
-  emailParts   = lens mailParts (\m p -> m {mailParts=p})
-
--- | Sends exactly one email.
---   This means multiple recipients will all get the same "to" header which has everyone
---   that the message is being sent to in it.
-data SingleEmail
-  = SingleEmail
-    { _seFrom        :: Address
-    , _seTo          :: [Address]
-    , _seCC          :: [Address]
-    , _seBCC         :: [Address]
-    , _seHeaders     :: Headers
-    , _seParts       :: [Alternatives]
-    , _seSendOptions :: MailgunSendOptions
-    }
-  deriving (Show)
-
-makeLenses ''SingleEmail
-
-instance IsEmail SingleEmail where
-  emailFrom    = seFrom
-  emailTo      = seTo
-  emailCC      = seCC
-  emailBCC     = seBCC
-  emailHeaders = seHeaders
-  emailParts   = seParts
-
-instance HasMailgunSendOptions SingleEmail where
-  mailgunSendOptions = seSendOptions
-
-{-
--- | Sends a seperate email to each recipient.
-data BroadcastEmail
- = BroadcastEmail
-   { _beFrom        :: Address
-   , _beTo          :: Map Address JS.Value
-   , _beCC          :: [Address]
-   , _beBCC         :: [Address]
-   , _beHeaders     :: Headers
-   , _beParts       :: [Alternatives]
-   , _beSendOptions :: MailgunSendOptions
-   }
- deriving (Show)
-
-makeLenses ''BroadcastEmail
-
-instance IsEmail BroadcastEmail where
-  emailFrom    = beFrom
-  emailTo      = beTo.to Map.keys
-  emailCC      = beCC
-  emailBCC     = beBCC
-  emailHeaders = beHeaders
-  emailParts   = beParts
-
-instance HasMailgunSendOptions BroadcastEmail where
-  mailgunSendOptions = beSendOptions
--}
-
-
-
-sendGeneric :: (HasMailgunConfig c, MonadIO m, MonadThrow m, MonadReader c m)
-            => [HTTP.Part] -> m MessageID
-sendGeneric m = do
-  t <- view mailgunTestMode
-  call (MGPost (printf "/v3/%s/messages") [] (yesNo "o:testmode" t:m))
-       (^?key "id"._JSON)
-
-class MailgunEmail mail where
-  {-# MINIMAL send #-}
-  send :: (HasMailgunConfig c, MonadIO m, MonadThrow m, MonadReader c m)
-       => mail -> m MessageID
-  sending :: (HasMailgunConfig c, MonadIO m, MonadThrow m, MonadReader c m)
-       => ProcessT m mail MessageID
-  sending = autoM send
--}
-
+      let sharedParts = mconcat $
+                        [ mgsoAsMultipart test mo
+                        , [partText "from" . renderAddress $ fromAddr]
+                        , map (partText "cc" . renderAddress)  ccAddr
+                        , map (partText "bcc" . renderAddress) bccAddr
+                        , [partText "subject" subj]
+                        , mergeTheseWith
+                              (pure . partText "html")
+                              (pure . partText "text")
+                              (++)
+                              theseBodies
+                        , map (attachmentToMutli True) inline
+                        , map (attachmentToMutli False) attach
+                        ]
+      pure . autoM $ \batch' -> do
+        let batch = Map.fromList . map (\tpl@(addr, _) -> (addressEmail addr, tpl)) $ batch'
+        call (MGPost (printf "/v3/%s/messages") [] . mconcat $
+               [ [partLBS "recipient-variables" . JS.encode .
+                    JS.toJSON . fmap snd $ batch
+                 ]
+               , (map (partText "to") . map (renderAddress . fst) . Map.elems $ batch)
+               , sharedParts
+               ])
+             (^?key "id"._JSON)
